4acfe443a705530682e1d52f416f123661cd2c6e
henrique-pimenta <94808259+henrique-pimenta@users.noreply.github.com>
2023-11-02 15:01:14 -0300

    fix(sign-up): add already registered account validation

--- sign_up/lambda_function.py
+++ sign_up/lambda_function.py
@@ -6,10 +6,16 @@ import redis
 
 
 def lambda_handler(event, context):
-    event["password"] = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto").hash(event["password"])
+    if not (event and event.get("cpf") and event.get("password")):
+        return {"statusCode": 400}
 
     redis_key = f'cpf:{event["cpf"]}'
+
+    if redis.Redis(host=os.environ["REDIS_URL"], port=6379, db=0).get(redis_key):
+        return {"statusCode": 401}
+
     del event["cpf"]
+    event["password"] = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto").hash(event["password"])
 
     redis.Redis(host=os.environ["REDIS_URL"], port=6379, db=0).set(redis_key, json.dumps(event))
 
